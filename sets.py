43d6b766ca809ae63bc5de6321b6eca12e9376eb
Fakhita123* <farhan.mustapha2016@gmail.com>
2024-10-07 22:00:53 +0100

    w avec set

---
+++ sets.py
@@ -0,0 +1,11 @@
+student_id=set()  #set
+
+student_id.add(123)  #pour ajouter des elements 
+student_id.add(223)
+student_id.add(755)
+
+print(223 in student_id) # true car exist
+print(100 in student_id) # fals car n'exist pas dans le set 
+
+# thanks for your attention 
+#you are haker now 
