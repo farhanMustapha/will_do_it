2c09a084771f93fe1b6c8a89e965d8e77fef7fdd
Fakhita123* <farhan.mustapha2016@gmail.com>
2024-10-07 21:36:31 +0100

    add qst

--- hash_fun.py
+++ hash_fun.py
@@ -2,6 +2,7 @@
 # ord convert this char to a number (unicode )
 # calculate the sum and put it in s
 # return s until le reste de la division par 10 egale zero then stop
+#the question is why and where i can use it hhh
 
 def hashing_funct(texto):
     s=[sum(ord(x) for x in texto)]
