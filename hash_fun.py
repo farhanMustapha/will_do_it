359749f05953d3620e9446b1c184b1a7f225181e
Fakhita123* <farhan.mustapha2016@gmail.com>
2024-10-07 21:47:57 +0100

    other way

--- hash_fun.py
+++ hash_fun.py
@@ -3,12 +3,19 @@
 # calculate the sum and put it in s
 # return s until le reste de la division par 10 egale zero then stop
 #the question is why and where i can use it hhh
-
+"""
 def hashing_funct(texto):
     s=sum(ord(x) for x in texto)
     return s % 10
 
-a=hashing_funct("world")
+"""
+def hashing_other_way(textiiii):
+    s=0
+    for x in textiiii:
+        s=s+ord(x)
+    return s % 10
+
+a=hashing_other_way("world")
 print(a)
 
 #it s ok if you didn't understand the point
