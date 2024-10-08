85ee05805d9a577c5ab529801e8139cdbcc3b7fe
Fakhita123* <farhan.mustapha2016@gmail.com>
2024-10-08 20:12:58 +0100

    second

--- couple.py
+++ couple.py
@@ -3,7 +3,7 @@ def chercher_position(L,a):
         if L[i]==a:
             return i
 
-
+#the proble here if we have duplicate algo return just the first index 
 b=[4,5,1,0,5,4,9]
 a=chercher_position(b,9)
 print(a)
