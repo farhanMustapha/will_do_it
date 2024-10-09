012430cf064dcec2fe80a5cc229947bea8f531d1
Fakhita123* <farhan.mustapha2016@gmail.com>
2024-10-09 21:13:09 +0100

    sort value

--- dictionnaire.py
+++ dictionnaire.py
@@ -1,4 +1,4 @@
-d={
+""" d={
     "aladin":[12,15,17],
     "natalie":[15,13,16],
     "robert":[13,15,11]
@@ -12,6 +12,22 @@ for k,v in d.items():
         k:moy_arrond
     }
 
-    print(dd)
+    print(dd) """
+
+d={
+    "a1":[21,17,22,3],
+    "a2":[11,15,8,13],
+    "a3":[7,13,2,11],
+    "a4":[22,14,7,9]
+}
+
+sorted_v={}
+for k,v in d.items():
+    v.sort()
+    sorted_v[k]=v
+
+print(sorted_v)
+
+    
     
     
