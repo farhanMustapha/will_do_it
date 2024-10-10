01561e32e8cd49703426eaee58123cbd88c7403a
Fakhita123* <farhan.mustapha2016@gmail.com>
2024-10-10 20:12:03 +0100

    remove item from set

--- tuple.py
+++ tuple.py
@@ -1,6 +1,14 @@
 A={'stylo','crayon','gorme'}
-b=input()
+""" b=input()
 if b in A:
     print("exist")
 else:
-    print("non trouver")
+    print("non trouver") """
+#print(len(A))
+A.add("regle")
+A.update(["atrouse","papier","stylo bleu"])
+try:
+    A.remove("atrous")
+    print(A)
+except :
+    print("n'existe pas ")
